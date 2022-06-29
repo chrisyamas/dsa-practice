af19a0ae40f29d7d319f102b3493c02262df2b67
Christopher Yamas <yamas.chris@gmail.com>
2022-06-29 14:48:25 -0400

    addition invent file create

---
+++ math_fun/invent_addition.py
@@ -0,0 +1,13 @@
+
+def invent_addition(num_1, num_2):
+    """
+    Function will take in two numbers and add them
+    without using any arithmetic operators.
+    """
+    add_dict = {}
+    for integer in range(0, 10):
+        int_dict = {}
+        for other_int in range(0, 10):
+            pass
+
+    # continue this later
