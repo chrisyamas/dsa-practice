3321afa05db4bf4ec2e76bb7bd164b2eeafb0b15
Christopher Yamas <yamas.chris@gmail.com>
2022-09-02 19:34:42 -0500

    binary search solution

--- math_fun/my_sqrt.py
+++ math_fun/my_sqrt.py
@@ -20,3 +20,20 @@ def log_my_sqrt(x):
     left = int(e ** (0.5 * log(x)))
     right = left + 1
     return left if right * right > x else right
+
+
+# Binary search solution
+def binary_search_my_sqrt(x):
+    if x < 2:
+        return x
+    left, right = 2, x // 2
+    while left <= right:
+        pivot = left + (right - left) // 2
+        num = pivot * pivot
+        if num > x:
+            right = pivot - 1
+        elif num < x:
+            left = pivot + 1
+        else:
+            return pivot
+    return right
