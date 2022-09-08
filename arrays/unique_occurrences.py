689603cffb94b21b25d0695798c6dba0386109de
Christopher Yamas <yamas.chris@gmail.com>
2022-09-07 22:40:51 -0500

    first failed attempt

---
+++ arrays/unique_occurrences.py
@@ -0,0 +1,12 @@
+
+
+# First Attempt: Failed (needs to be debugged)
+def uniqueOccurrences(arr):
+    count_hash = {}
+    for x in arr:
+        if x in count_hash:
+            count_hash[x] += 1
+        else:
+            count_hash[x] = 1
+    counts_list = list(count_hash.keys())
+    return counts_list == set(counts_list)
